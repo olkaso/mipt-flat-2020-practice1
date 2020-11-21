94c69edfd05aa743e77d88e669dd4c0b4a5efdeb
olkaso <sofronova.oi@phystech.edu>
2020-11-21 17:57:46 +0300

    code style fixed

--- main.py
+++ main.py
@@ -1,59 +1,80 @@
-LTR_ALPH = {'a', 'b', 'c', '1'}
+def apply_plus(desired_count, lhs, rhs):
+    res = [False for i in range(desired_count + 1)]
+    for i in range(desired_count + 1):
+        res[i] = lhs[i] or rhs[i]
+    return res
 
 
-def process(reg, ltr, cnt):
+def apply_point(desired_count, lhs, rhs):
+    res = [False for i in range(desired_count + 1)]
+    for i in range(desired_count + 1):
+        for j in range(desired_count + 1):
+            if lhs[i] and rhs[j] and i + j < desired_count+1:
+                res[i+j] = True
+    return res
+
+
+def apply_star(desired_count, possible_count_array):
+    res = [False for i in range(desired_count + 1)]
+    res[0] = True
+    counts_list = []
+    for i in range(desired_count + 1):
+        if possible_count_array[i]:
+            counts_list.append(i)
+    for i in range(desired_count + 1):
+        if res[i]:
+            for count in counts_list:
+                if i + count < desired_count + 1:
+                    res[i + count] = True
+    return res
+
+
+def create_count_array(desired_count, symbol, letter):
+    res = [symbol != letter, symbol == letter]
+    for i in range(desired_count - 1):
+        res.append(False)
+    return res
+
+
+def process(regexp, letter, desired_count):
     st = []
-    for ch in reg:
-        if ch in LTR_ALPH:
-            lst = [ch != ltr, ch == ltr]
-            for i in range(cnt-1):
-                lst.append(False)
-            st.append(lst)
+    for ch in regexp:
+        if ch in {'a', 'b', 'c', '1'}:
+            st.append(create_count_array(desired_count, ch, letter))
         else:
             if len(st) == 0:
                 return 2
-            cnt2 = st.pop()
-            res = [False for i in range(cnt+1)]
+            lhs = st.pop()
             if ch == '*':
-                for i in range(cnt+1):
-                    if i == 0:
-                        res[0] = True
-                    elif cnt2[i]:
-                        k = 1
-                        while k*i < cnt + 1:
-                            res[k*i] = True
-                            k += 1
-                st.append(res)
+                st.append(apply_star(desired_count, lhs))
                 continue
             if len(st) == 0:
                 return 2
-            cnt1 = st.pop()
+            rhs = st.pop()
             if ch == '.':
-                for i in range(cnt+1):
-                    for j in range(cnt+1):
-                        if cnt1[i] and cnt2[j] and i + j < cnt+1:
-                            res[i+j] = True
-                st.append(res)
-                continue
-            if ch == '+':
-                for i in range(cnt+1):
-                    res[i] = cnt1[i] or cnt2[i]
-                st.append(res)
+                st.append(apply_point(desired_count, lhs, rhs))
+            elif ch == '+':
+                st.append(apply_plus(desired_count, lhs, rhs))
     if len(st) != 1:
         return 2
     return st[0][-1]
 
 
-inp = input().split()
+def main():
+    inp = input().split()
 
-reg = inp[0]
-ltr = inp[1]
-cnt = int(inp[2])
+    regexp = inp[0]
+    letter = inp[1]
+    desired_count = int(inp[2])
 
-ans = process(reg, ltr, cnt)
-if ans == 2:
-    print("ERROR")
-elif ans:
-    print("YES")
-else:
-    print("NO")
+    return_code = process(regexp, letter, desired_count)
+    if return_code == 2:
+        print("ERROR")
+    elif return_code:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    main()
